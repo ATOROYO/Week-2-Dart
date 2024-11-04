8d82abd26d0f8013dda039a25d7a7c7281fe117c
ATOROYO <davidatoroyosika@gmail.com>
2024-11-04 16:42:21 +0300

    Wrote funtion with parameters and return type

--- addTwoNumbers.dart
+++ addTwoNumbers.dart
@@ -17,6 +17,11 @@ String InstructorName() {
   return "Allan";
 }
 
+//  Function With Parameter And Return Type
+int add(int a, int b) {
+  return a + b;
+}
+
 void main() {
   findSum(10, 5.5);
 
@@ -25,4 +30,10 @@ void main() {
 
   // Print the instructor's name
   print("The instructor's name is: $instructor");
+
+  // Calling the function with two integers
+  int result = add(10, 20);
+
+  // Print he result
+  print("The sum of 10 and 20 is: $result");
 }
