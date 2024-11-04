cb6e4df75adef395719e0078046c71392dd4ed6f
ATOROYO <davidatoroyosika@gmail.com>
2024-11-04 15:57:49 +0300

    Wrote Function To Find Sum of Two Numbers

--- funtion.dart
+++ funtion.dart
@@ -8,8 +8,15 @@ void printName2() {
   print("My name is Daisy");
 }
 
+// Example 2: Function To Find Sum of Two Numbers
+void findSum(int num1, int num2) {
+  int sum = num1 + num2;
+  print("The sum of $num1 and $num2 is: $sum");
+}
+
 void main() {
   // Callimng the function
   printName();
   printName2();
+  findSum(10, 20);
 }
