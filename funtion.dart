ca9cee4304874365d930e2cb616c45696f637fca
ATOROYO <davidatoroyosika@gmail.com>
2024-11-04 15:53:48 +0300

    Deleted some codes

--- funtion.dart
+++ funtion.dart
@@ -3,11 +3,6 @@ void printName() {
   print("Brenda");
 }
 
-// Call the function
-void main() {
-  printName();
-}
-
 // Example 2
 void printName2() {
   print("My name is Daisy");
@@ -15,5 +10,6 @@ void printName2() {
 
 void main() {
   // Callimng the function
+  printName();
   printName2();
 }
