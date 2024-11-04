04e02dac4b0e22c84187900a7dc204b7d74d8ccd
ATOROYO <davidatoroyosika@gmail.com>
2024-11-04 18:46:17 +0300

    Did a simple example

---
+++ arrowFunctions.dart
@@ -0,0 +1,9 @@
+// Example 1: Calculation of simple interest without Arrow Function
+
+// Simple arrow function to print name
+void printName() => print("My name is Mariam");
+
+void main() {
+  // Calling the arrow function
+  printName();
+}
