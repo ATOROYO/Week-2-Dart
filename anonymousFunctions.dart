e573647408db9c1d5c1f042b2202b87373dcda69
ATOROYO <davidatoroyosika@gmail.com>
2024-11-04 18:18:29 +0300

    Did example 1

---
+++ anonymousFunctions.dart
@@ -0,0 +1,21 @@
+// Function to print name
+// void printName(){
+//   print("My name is Nyuti");
+// }
+
+// void main(){
+//   // Calling the function
+//   printName();
+// }
+
+// Anonymous Function to print name
+void main() {
+  // Anonymous function assigned to the variable `printName`
+  var printName = () {
+    // Print message inside the anonymous function
+    print("My name is Nyuti");
+  };
+
+  // Call the anonymous function
+  printName();
+}
